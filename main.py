18ab182e5e51ecd5862c06ab81295654ca728b7a
zThundy <antonioantonelli7@gmail.com>
2021-09-21 22:37:27 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys, pygame
+
+size = width, height = 550, 550
+name = "Prova"
+
+def main(args):
+    pygame.init()
+    DISPLAY = pygame.display.set_mode(size, 0, 32)
+    pygame.display.set_caption(name)
+    pygame.display.set_icon("./sprites/test.png")
+
+    # use font pygame
+    font = pygame.font.Font("./fonts/game_over.ttf", 100)
+    
+    # get player skin
+    player = pygame.image.load("./sprites/pg.png")
