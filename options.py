31c9ba27334d06ca8fd69a85bb9d4d108701b042
zThundy <antonioantonelli7@gmail.com>
2021-09-22 22:28:31 +0200

    merge of options and sound class

--- options.py
+++ options.py
@@ -1,15 +1,49 @@
+import pygame
+
 class Options:
     options = {
-        "sound": True,
+        "sound": False,
         "music": False,
         "fullscreen": False,
         "vsync": False
     }
 
     def getValue(self, elem):
-        if self.options[elem]:
-            return self.options[elem]
+        return self.options[elem]
 
     def setValue(self, elem, value):
-        if self.options[elem]:
-            self.options[elem] = value
+        self.options[elem] = value
+
+class Sounds(Options):
+    musicPlaying = False
+    currentSound = 0
+    volume = 0.3
+
+    def playSound(self, path):
+        if Options.getValue(self, "sound"):
+            self.currentSound = pygame.mixer.Sound(path)
+            pygame.mixer.Sound.play(self.currentSound)
+            self.currentSound.set_volume(self.volume)
+
+    def playMusic(self, path, loop):
+        if Options.getValue(self, "music"):
+            if self.musicPlaying:
+                self.stopMusic()
+            pygame.mixer.music.load(path)
+            pygame.mixer.music.set_volume(self.volume)
+            if loop:
+                pygame.mixer.music.play(-1)
+            else:
+                pygame.mixer.music.play()
+            self.musicPlaying = True
+
+    def stopMusic(self):
+        self.musicPlaying = False
+        pygame.mixer.music.stop()
+        pygame.mixer.music.unload()
+
+    def changeVolume(self, volume):
+        if volume >= 0.1 and volume <= 1.0:
+            if pygame.mixer.get_busy():
+                self.volume = volume
+                self.currentSound.set_volume(self.volume)
